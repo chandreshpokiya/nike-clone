e3a44b33f108902b8a95bfc89c55de2aecadaf34
chandresh pokiya <chandreshpokiya6574@gmail.com>
2023-03-22 09:41:00 +0530

    initial commit

---
+++ components/ProductCard.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const ProductCard = () => {
+   return (
+      <Link href={"/product/1"}  className="transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
+         {/* eslint-disable-next-line */}
+         <img className="w-full" src="/product-1.webp" alt="product" />
+         <div className="p-4 text-black/[0.9]">
+            <h2 className="text-lg font-medium">product name</h2>
+            <div className="flex items-center text-black/[0.5]">
+               <p className="mr-2 text-lg font-semibold">&#8377; 10000</p>
+               <p className="text-base font-medium line-through">&#8377; 100000</p>
+               <p className="ml-auto text-base font-medium text-green-500">90% off</p>
+            </div>
+         </div>
+      </Link>
+   );
+};
+
+export default ProductCard;
